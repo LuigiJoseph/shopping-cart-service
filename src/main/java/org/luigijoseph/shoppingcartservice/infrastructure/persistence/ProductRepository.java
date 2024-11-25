fe9ef92779499cead75c09a8e5affc8edcde413f
Mohammed <luigiornojoseph@gmail.com>
2024-11-25 21:46:04 +0300

    Made Jpa repositories of entities

--- src/main/java/org/luigijoseph/shoppingcartservice/infrastructure/persistence/ProductRepository.java
+++ src/main/java/org/luigijoseph/shoppingcartservice/infrastructure/persistence/ProductRepository.java
@@ -1,10 +1,9 @@
 package org.luigijoseph.shoppingcartservice.infrastructure.persistence;
 
-import org.luigijoseph.shoppingcartservice.domain.entities.ShoppingCart;
+import org.luigijoseph.shoppingcartservice.domain.entities.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CartRepository extends JpaRepository<ShoppingCart, Long> {
-
-    }
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
--- src/main/java/org/luigijoseph/shoppingcartservice/infrastructure/persistence/StoreRepository.java
+++ src/main/java/org/luigijoseph/shoppingcartservice/infrastructure/persistence/StoreRepository.java
@@ -1,4 +1,9 @@
 package org.luigijoseph.shoppingcartservice.infrastructure.persistence;
 
-public class StoreRepository {
+import org.luigijoseph.shoppingcartservice.domain.entities.Store;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StoreRepository extends JpaRepository<Store, Long> {
 }
